6c1c54e980b418852451b750fc0358bea1dea80e
CunhazadaNoDale <cunha12324@gmail.com>
2026-08-17 22:44:05 -0300

    feat: repositorio para o dominio

---
+++ command-service/src/main/java/org/command/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package org.command.domain.usuario;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsuarioRepository {
+    Optional<Usuario> buscarPorId(UUID userId);
+    Usuario criar(Usuario usuario);
+    Boolean usuarioExiste(UUID userId);
+}
