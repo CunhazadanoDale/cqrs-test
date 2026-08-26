bcd7ad6d8b0b5e5b0ad383755160bd74d0292ba1
CunhazadaNoDale <cunha12324@gmail.com>
2026-08-26 20:12:15 -0300

    feat: criação do metodo no handler

--- command-service/src/main/java/org/command/application/usuario/CriarUsuarioHandler.java
+++ command-service/src/main/java/org/command/application/usuario/CriarUsuarioHandler.java
@@ -1,4 +1,32 @@
 package org.command.application.usuario;
 
+import org.command.domain.usuario.Usuario;
+import org.command.domain.usuario.UsuarioRepository;
+
+import java.util.UUID;
+
 public class CriarUsuarioHandler {
+    private final UsuarioRepository usuarioRepository;
+
+    public CriarUsuarioHandler(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public UUID handle(CriarUsuarioCommand command) {
+        String username = command.username().trim();
+        if (username.equals("") || username == null) {
+            throw new IllegalArgumentException("Username nao pode ser vazio");
+        }
+
+        if(usuarioRepository.usuarioExistePorUsername(username)) {
+            throw new RuntimeException("Usuario ja existe");
+        }
+
+        Usuario usuario = Usuario.criar(username, command.email(),
+                command.senhaHash(), command.bio());
+
+        usuarioRepository.criar(usuario);
+
+        return usuario.getId();
+    }
 }
